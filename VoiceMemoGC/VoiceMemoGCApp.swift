a068e4dc5cabc7d5da525314edd23ba1a8b33225
gcaiazzo9 <giorgiocaiazzo@icloud.com>
2023-11-13 21:59:11 +0100

    Initial Commit

---
+++ VoiceMemoGC/VoiceMemoGCApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoGCApp.swift
+//  VoiceMemoGC
+//
+//  Created by Giorgio Caiazzo on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoGCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
